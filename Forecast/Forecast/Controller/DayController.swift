b8beedd0ba06f330134f3dab78928d6f840b284c
Isiah Parra <isiahparra@Isiahs-MacBook-Pro.local>
2022-05-24 17:40:17 -0600

    Finished networking code for API endpoint

--- Forecast/Forecast/Controller/DayController.swift
+++ Forecast/Forecast/Controller/DayController.swift
@@ -19,15 +19,39 @@ class DayController {
         let dailyURL = forecastURL.appendingPathComponent("daily")
         // Query URLs
         var urlComponents = URLComponents(url: dailyURL, resolvingAgainstBaseURL: true)
-        var apiQuery = URLQueryItem(name: "key", value: "8503276d5f49474f953722fa0a8e7ef8")
-        var cityQuery = URLQueryItem(name: "city", value: "miami")
-        var unitsQuery = URLQueryItem(name: "units", value: "I")
+        let apiQuery = URLQueryItem(name: "key", value: "8503276d5f49474f953722fa0a8e7ef8")
+        let cityQuery = URLQueryItem(name: "city", value: "miami")
+        let unitsQuery = URLQueryItem(name: "units", value: "I")
         urlComponents?.queryItems = [apiQuery,cityQuery,unitsQuery]
         guard let finalURL = urlComponents?.url else {return}
         print(finalURL)
+        URLSession.shared.dataTask(with: finalURL) { dayData, _, error in
+            if let error = error {
+                print("There was an error fetching the data. The url is \(finalURL), the error is \(error.localizedDescription)")
+                completion(nil)
+            }
+            guard let data = dayData else {
+                print("There was an error retrieving the data!")
+                completion(nil)
+                return
+            }
+            do {
+                if let topLevelDictionary = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed) as? [String:Any] {
+                    let cityName = topLevelDictionary["city_name"] as! String
+                    let dataArray = topLevelDictionary["data"] as! [[String:Any]]
+                    
+                    var tempDayArray: [Day] = []
+                    for dayDictionary in dataArray {
+                        guard let day = Day(dayDictionary: dayDictionary, cityName: cityName) else {return}
+                        tempDayArray.append(day)
+                    }
+                    completion(tempDayArray)
+                }
+            }
+            catch {
+                print("Error in Do/Try/Catch: \(error.localizedDescription)")
+                completion(nil)
+            }
+        }.resume()
     }
-    
-    
-    
-    
 }// end of class
